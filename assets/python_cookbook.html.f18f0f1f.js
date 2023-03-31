df96d2879d9143563b0713ecaba0d323301b962c
Igarashi-G <2545369032@qq.com>
2023-03-31 14:50:51 +0000

    Deploying to gh-pages from @ Igarashi-G/Igarashi-G.github.io@ba0949ab100414b8ea942678ded72177b23c4219 🚀

---
+++ assets/python_cookbook.html.f18f0f1f.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./plugin-vueexport-helper.2444895f.js";import{o as e,c as n,d as c,r}from"./app.dc8bd00f.js";const _={};function s(p,a){const o=r("PDF");return e(),n("div",null,[c(o,{url:"https://www.igarashi.fun:7779/pdf/python/Python%20Cookbook.pdf",height:"880px"})])}const l=t(_,[["render",s],["__file","python_cookbook.html.vue"]]);export{l as default};
