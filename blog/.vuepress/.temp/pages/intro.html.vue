319aff667af059c5d53c2da4f6cad8400d981413
Igarashi <2545369032@qq.com>
2023-10-06 23:28:17 +0800

    rebuild

--- blog/.vuepress/.temp/pages/intro.html.vue
+++ blog/.vuepress/.temp/pages/intro.html.vue
@@ -11,37 +11,37 @@
 -->
 <h3 id="language" tabindex="-1"><a class="header-anchor" href="#language" aria-hidden="true">#</a> Language</h3>
 <a href="https://www.python.org/">
-  <img src="https://www.igarashi.icu:7779/img/python.png" alt="Python" height="50"/>
+  <img src="https://www.igarashi.icu:8999/img/python.png" alt="Python" height="50"/>
 </a>
 <a href="https://www.javascript.com/">
-  <img src="https://www.igarashi.icu:7779/img/js.png" alt="JS" height="50"/>
+  <img src="https://www.igarashi.icu:8999/img/js.png" alt="JS" height="50"/>
 </a>
 <h3 id="framework" tabindex="-1"><a class="header-anchor" href="#framework" aria-hidden="true">#</a> Framework</h3>
 <a href="https://fastapi.tiangolo.com/">
-  <img src="https://www.igarashi.icu:7779/img/fastapi.png" alt="FastApi" height="50"/>
+  <img src="https://www.igarashi.icu:8999/img/fastapi.png" alt="FastApi" height="50"/>
 </a>
 <a href="https://www.djangoproject.com/">
-  <img src="https://www.igarashi.icu:7779/img/Django.png" alt="Django" height="50"/>
+  <img src="https://www.igarashi.icu:8999/img/Django.png" alt="Django" height="50"/>
 </a>
 <a href="https://www.tornadoweb.org/">
-  <img src="https://www.igarashi.icu:7779/img/tornado.png" alt="Tornado" height="50"/>
+  <img src="https://www.igarashi.icu:8999/img/tornado.png" alt="Tornado" height="50"/>
 </a>
 <br/>
 <a href="https://v3.cn.vuejs.org/">
-  <img src="https://www.igarashi.icu:7779/img/vue.png" alt="Vue" height="50"/>
+  <img src="https://www.igarashi.icu:8999/img/vue.png" alt="Vue" height="50"/>
 </a>
 <a href="https://quasar.dev/">
-  <img src="https://www.igarashi.icu:7779/img/quasar.png" alt="quasar" height="50"/>
+  <img src="https://www.igarashi.icu:8999/img/quasar.png" alt="quasar" height="50"/>
 </a>
 <a href="https://facebook.github.io/react/">
-  <img src="https://www.igarashi.icu:7779/img/react.png" alt="React" height="50"/>
+  <img src="https://www.igarashi.icu:8999/img/react.png" alt="React" height="50"/>
 </a>
 <h3 id="my-profile" tabindex="-1"><a class="header-anchor" href="#my-profile" aria-hidden="true">#</a> My profile</h3>
 <a href="https://github.com/Igarashi-G">
   <img height="156em" src="https://bad-apple-github-readme.vercel.app/api?show_bg=1&username=Igarashi-G&show_icons=true" alt="Igarashi's GitHub Stats" />
   <img height="156em" src="https://github-readme-stats.vercel.app/api/top-langs/?username=Igarashi-G&hide=html,less&theme=radical&layout=compact" />
 </a>
-<img src="https://www.igarashi.icu:7779/img/qb.gif" alt="魔法少女QB" height="60" align="right"/>
+<img src="https://www.igarashi.icu:8999/img/qb.gif" alt="魔法少女QB" height="60" align="right"/>
 <p><strong>Email:</strong> <a href="mailto:igarashifk@gmail.com">igarashifk@gmail.com</a> 💌</p>
 <p>坐标 <strong>深圳</strong>, 有空一起玩~ 🎮</p>
 </div></template>
